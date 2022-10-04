cb83f81c0d470247283f27c4125add7e378e2da2
Veronika Sergienko <nichka030791@yandex.ru>
2022-10-04 22:47:47 +0600

    пр 13. настройка обработки ошибок 08

--- errors/NotFound.js
+++ errors/NotFound.js
@@ -2,6 +2,7 @@ class NotFound extends Error {
   constructor(message) {
     super(message);
     this.name = 'NotFound';
+    this.message = message;
     this.status = 404;
   }
 }
--- errors/ValidationError.js
+++ errors/ValidationError.js
@@ -2,6 +2,7 @@ class ValidationError extends Error {
   constructor(message) {
     super(message);
     this.name = 'ValidationError';
+    this.message = message;
     this.status = 400;
   }
 }
