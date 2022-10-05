c77ec9624aee4c76640f7911f673f655c3e6bcd1
Veronika Sergienko <nichka030791@yandex.ru>
2022-10-05 16:35:21 +0600

    пр 13. исправления после ревью 03

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-param-reassign, no-underscore-dangle */
 const Cards = require('../models/card');
 const NotFound = require('../errors/NotFound');
-const ValidationError = require('../errors/ValidationError');
 
 // GET /cards — возвращает все карточки
 const getCard = (req, res) => {
@@ -15,11 +14,10 @@ const createCard = (req, res) => {
   const { name, link } = req.body;
   const ownerId = req.user._id;
   Cards.create({ name, link, owner: ownerId })
-    .orFail(new ValidationError('Переданы некорректные данные при создании карточки'))
     .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(err.status).send({ message: err.message });
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
       } else {
         res.status(500).send({ message: 'На сервере произошла ошибка' });
       }
