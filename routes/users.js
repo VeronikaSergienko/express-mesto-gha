4235ceda562aaba37619ef0353ec29a213546658
Veronika Sergienko <nichka030791@yandex.ru>
2022-10-20 22:07:01 +0600

    исправления после ревью 1-4

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,8 @@ router.get('/', getUser);
 router.get('/me', getProfile);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(false).min(2).max(30),
-    about: Joi.string().required(false).min(2).max(30),
-    avatar: Joi.string().regex(/http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9aFA-F]))+/),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 }), patchUserId);
 router.patch('/me/avatar', celebrate({
@@ -22,7 +21,7 @@ router.patch('/me/avatar', celebrate({
 }), patchUserAvatar);
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().alphanum().length(24).hex(),
   }),
 }), getUserId);
 
