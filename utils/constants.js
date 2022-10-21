73ac8f6d5f3a26d2921a168e086b8cee66ea4ddc
Veronika Sergienko <nichka030791@yandex.ru>
2022-10-21 23:32:44 +0600

    исправления после ревью 1-13

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,3 @@
-const regExpURL = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9aFA-F]))+/;
-
-// /^https?:\/\/[\w\/?.&-=]+$/gi;
+const regExpURL = /^https?:\/\/[\w\/?.&-=]+$/;
 
 module.exports = { regExpURL };
