6a44ec8940ac4267baabeeeefbf66b468034db73
Veronika Sergienko <nichka030791@yandex.ru>
2022-10-21 23:05:40 +0600

    исправления после ревью 1-10

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,8 @@ const {
   getUser, getUserId, patchUserId, patchUserAvatar, getProfile,
 } = require('../controllers/users');
 
-const regExpURL = require('../utils/constants');
+// const regExpURL = require('../utils/constants');
+const regExpURL = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9aFA-F]))+/;
 
 router.get('/', getUser);
 
